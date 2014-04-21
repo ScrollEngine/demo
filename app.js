df6cb4066173946a387b21500ba400b3908303ee
Matt Hernandez <matt@modulus.io>
2014-04-20 21:12:26 -0400

    Cleanup.
    Removed the unused views; fixed the main property; fixed the indentation for
    the app.

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var scroll = require('scroll-core');
-    app = scroll({
-      model: {
-        connection: __dirname + '/test/' //path to JSON data
-      }
-    });
+var app = scroll({
+  model: {
+    connection: __dirname + '/test/' //path to JSON data
+  }
+});
 
 app.start(8181);
